9ecd1a4cf928dd0c407562b6a175dcf0da0f366c
shounakray0710 <85279032+shounakray0710@users.noreply.github.com>
2021-08-05 19:23:25 +0530

    Update stone.js

--- js/stone.js
+++ js/stone.js
@@ -1,24 +1,23 @@
 class Stone {
-    constructor(x, y, w,h) 
+    constructor(x, y, r) 
 
     {
         var stone_options={
              density: 0.001
         };
         
-        stone  = Bodies.circle(300,300,15,stone_options)
-        this.w = w;
-        this.h = h;
-        World.add(world, this.body);
+        this.stone  = Bodies.circle(x,y,r,stone_options)
+      this.r = r;
+        World.add(world, this.stone);
       }
     
       show() {
-        let pos = this.body.position;
+        let pos = this.stone.position;
         push();
         ellipseMode(RADIUS);
         noStroke();
         fill(148,127,146);
-        ellipse(fruit.position.x,fruit.position.y,15,15)
+        ellipse(pos.x,pos.y,this.r,this.r)
         pop();
       }
     }
