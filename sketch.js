09a998661d6ec2fa71aa7f9b270d14c008882d10
shounakray0710 <85279032+shounakray0710@users.noreply.github.com>
2021-08-05 19:24:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,8 +21,10 @@ function setup() {
   frameRate(80);
 
   base = new Base (200,680,600,20);
-leftWall = new LeftWall(300,height/2+50,600,100,"#8d6e63",true);
-rightWall = new RightWall(width - 300,height/2+50,600,100,"#8d6e63",true);
+//leftWall = new LeftWall(300,height/2+50,600,100,"#8d6e63",true);
+//rightWall = new RightWall(width - 300,height/2+50,600,100,"#8d6e63",true);
+leftWall = new Base(300, height / 2 + 50, 300, 100);
+ rightWall = new Base(width - 450, height / 2 + 50, 300, 100);
 
 bridge = new Bridge (15,{x: width/2-400,y:height/2});
 jointPoint = new Base(width - 600,height/2+10,40,20,"#8d6e63",true);
@@ -33,8 +35,8 @@ jointLink = new Link(bridge,jointPoint);
 for (var i = 0; i<= 8;i++){
   var x = random(width/2-100,width/2+300);
   var y = random(-10,140);
-  var stone = new Stone ( X, Y ,80,80);
-  stones.push(Stone);
+  var stone = new Stone ( x, y ,30);
+  stones.push(stone);
 }
 
  
@@ -44,10 +46,10 @@ function draw() {
   background(51);
   Engine.update(engine);
 
-  ground.show();
+  base.show();
   bridge.show();
-  LeftWall.show();
-  RightWall.show();
+  leftWall.show();
+  rightWall.show();
 
 for(var stone of stones){
   stone.show();
